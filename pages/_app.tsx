9b546f5c78fb44af36487fb29b87e68cecdee437
unknown <mada.lawliet@gmail.com>
2023-01-25 12:43:03 +0700

    Added redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <Provider store={store}>
       <Component {...pageProps} />
+      </Provider>
   );
 }
 
---
+++ redux/apiSlice.ts
@@ -0,0 +1,30 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+interface Repo {
+  id: number;
+  name: string;
+  full_name: string;
+  private: boolean;
+  html_url: string;
+  description: string;
+}
+
+type Repos = Repo[];
+
+// Define a service using a base URL and expected endpoints
+export const githubApi = createApi({
+  reducerPath: "githubApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://api.github.com/users/",
+  }),
+  endpoints: (builder) => ({
+    getUserRepos: builder.query<Repos, string>({
+      query: (username: string) => `${username}/repos`,
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetUserReposQuery } = githubApi;
